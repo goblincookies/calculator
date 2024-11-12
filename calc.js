4f1a7696c4a7ceb44b49637b35088393d22959bf
goblincookies <goblincookiebox@gmail.com>
2024-11-11 20:53:32 -0500

    lines shift up with calculations

--- calc.js
+++ calc.js
@@ -1,10 +1,10 @@
 let keys = document.querySelectorAll(".key");
-let userInput = document.querySelector("#user-input");
+let typeBox = document.querySelector("#user-input");
 let inputA = document.querySelector("#input-a");
+let inputB = document.querySelector("#input-b");
+let printLines = document.querySelector("#print-out").querySelectorAll("div");
 
-
-let values = "";
-// let curIndex=0;
+let userInput = "";
 
 let cursorVisible = true;
 let displayingAnswer = false;
@@ -26,14 +26,29 @@ function cursorVisibility( val ) {
     cursor.style.opacity = val+0;
     cursorVisible = val;
 }
+function userInputClear() {
+    userInput = "";
+    typeBox.textContent = userInput;
+}
+function userInputAdd( val) {
+    userInput += val;
+    typeBox.textContent = userInput;
+}
+function userInputDelete() {
+    userInput = userInput.slice(0, -1);
+    typeBox.textContent = userInput;
+}
 
 function keyPress(e) {
 
     if (displayingAnswer) {
+        shiftUp()
+        inputB.textContent="";
+        typeBox.classList.remove("yellow");
+        typeBox.classList.add("green");
         displayingAnswer = false;
         inputA.classList.remove("right");
-        userInput.textContent = "";
-        values = "";
+        userInputClear();
     }
     console.log( e.target.id );
     let isNumber = true;
@@ -69,13 +84,13 @@ function parseVal( val) {
     }
 
     // CHECK IF PREV IS THE SAME
-    console.log( "prev is: " + values[values.length-1])
+    console.log( "prev is: " + userInput[userInput.length-1])
     console.log( "this is: " + val )
 
-    if (values[values.length-1] != val ) {
+    if (userInput[userInput.length-1] != val ) {
 
         // CHECK PREV IS NOT A SYMBOL
-        if ( !isNaN( parseInt(values[values.length-1]))) {
+        if ( !isNaN( parseInt(userInput[userInput.length-1]))) {
 
             return val;
         } else {
@@ -89,33 +104,45 @@ function parseVal( val) {
 
 function addCharacter( val ) {
     val = parseVal( val)
-
-    values += val;
-    userInput.textContent += val;
+    userInputAdd(val);
 }
 
 function deleteCharacter () {
     // CHECK IF THERES MORE THAN ONE CHARACTER
     // SIMPLE DELETE
-    console.log(values)
-    values = values.slice(0-1);
-    userInput.textContent = userInput.textContent.slice(0,-1);
-    // if ( values.length > 0) {
-    // }
+    console.log(userInput);
+    userInputDelete();
+    // typeBox.textContent = typeBox.textContent.slice(0,-1);
 }
 function clearScreen() {
-    values = "";
-    userInput.textContent = "";
+    userInputClear();
+    for (let i= 0; i < printLines.length-3; i++) {
+        printLines[i].textContent = "";
+    }
 }
-// function updateLine() {
-// }
+
+function shiftUp() {
+    for (let i= 0; i < printLines.length-2; i++) {
+        if (printLines[i+2].childElementCount > 0 ) {
+            // SKIP THE CURSOR
+            printLines[i].textContent = printLines[i+2].children[0].textContent
+        } else {
+            // SIMPLE COPY UP
+            printLines[i].textContent = printLines[i+2].textContent
+        }
+    }
+}
+
 function calcSolution() {
     displayingAnswer = true;
     cursorVisibility(false);
+    inputB.textContent = userInput;
+    typeBox.classList.remove("green");
+    typeBox.classList.add("yellow");
     inputA.classList.add("right");
-    userInput.textContent = "hello!";
-    let numbers = values.split(/[/*-+%]/).filter(i=>i)
-    let operations = values.split(/[0-9]/).filter(i => i);;
+    typeBox.textContent = "hello!";
+    let numbers = userInput.split(/[/*-+%]/).filter(i=>i)
+    let operations = userInput.split(/[0-9]/).filter(i => i);;
     console.log("the numbers are " + numbers);
     console.log("the operations are " + operations);
 }
