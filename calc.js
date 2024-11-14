ad3fa81c12115ab7c662d58b0a5b4d786224ce81
goblincookies <goblincookiebox@gmail.com>
2024-11-14 13:46:18 -0500

    refactors input parsing for easier debuging

--- calc.js
+++ calc.js
@@ -38,15 +38,12 @@ function clearAnim() {
     });
 }
 function playShiftAnim() {
-    console.log("playing anim..")
-
-    printLines.forEach(el => {
+    printLines.forEach( el => {
         el.querySelector(".print-display").classList.add("shift-anim");
     });
 }
 
 function keyPress(e) {
-    console.log(e.key);
     let val = e.key;
 
     switch( val) {
@@ -59,7 +56,8 @@ function keyPress(e) {
             val = "calc";
             break;
         default:
-            val = val.split(/[^\d*+-/%.]/).filter(i=>i);
+            val = val[0];
+            // val = val.split(/[^\d*+-/%.]/).filter(i=>i);
             if (val.length > 0) {
                 val = val[0];
             } else {
@@ -103,7 +101,6 @@ function filterDigit( val ) {
         userInputClear();
     }
 
-    console.log( val );
     let isNumber = true;
 
     switch(val) {
@@ -123,116 +120,125 @@ function filterDigit( val ) {
 
     if (isNumber) {
         cursorVisibility(false);
-        addkeyClick( val);
+        addKeyClick( val);
     }
 
 };
 
-function makeHighlighSpan() {
-    let span = document.createElement("span");
-    span.classList.add("yellow");
-    return span;
-}
+function getOpperatorsSoFar() { return(userInput.split(/[0-9%.-]/).filter(i => i));}
+function getSymbolsUsedSoFar( val ) {return(userInput.split("").filter(i=>i==val) );}
+function isNumber( val) {return /^\d/.test(val);}
 
-function parseVal( val) {
-    // IT'S A NUMBER
-    if (parseInt(val).toString() == val ) {
-        return val;
+function qualifiedDigit(val) {
+    if (userInput.slice(-1) != "%") {
+        return true;
     }
-    // CHECK IF PREV IS THE SAME
-    console.log( "prev is: " + userInput[userInput.length-1])
-    console.log( "this is: " + val )
+    return false;
+}
+function qualifiedPercent(val) {
+    if (userInput.length>1) {
+        let lastDigit = userInput.slice(-1);
+        if ( lastDigit == "%") {
+            return true;
+        }
+        if (lastDigit==".") {
+            return true;
+        }
+        if(isNumber(lastDigit) ) {
+            return true;
+        }
+    }   
+    return false;
+}
+function qualifiedDecimal(val) {
+    if (userInput.slice(-1) != "%"){
+        let decimalsSoFar = getSymbolsUsedSoFar(".");
 
-    if (userInput[userInput.length-1] != val ) {
+        if (decimalsSoFar.length <1) {
+            return true;
+        }
+        let opperatorsSoFar = getOpperatorsSoFar();
+        if (decimalsSoFar.length < 2 & opperatorsSoFar.length > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+function qualifiedNegative(val) {
+    console.log("neg check");
+    // IS FIRST DIGIT
+    if ( userInput.length < 1 ) {
+        console.log("passed 1")
+        return true;
+    }
+    let lastDigit = userInput.slice(-1);
+    // LAST DIGIT IS SYMBOL
+    if (["+","*","/"].some( el => lastDigit.includes(el) ) ) {
+        console.log("passed 2")
 
-        // CHECK PREV IS NOT A SYMBOL
-        if ( !isNaN( parseInt(userInput[userInput.length-1]))) {
-
-            return val;
-        } else {
+        return true;
+    }
+    let negativesSoFar = getSymbolsUsedSoFar("-");
+    let opperatorsSoFar = getOpperatorsSoFar();
+    // LAST DIGIT IS A NUMBER, AND LESS THAN 2 NEGS, AND NO OPPERATORS 
+    if (isNumber(lastDigit)  && negativesSoFar.length<2 && opperatorsSoFar.length < 1) {
+        console.log("passed 3")
+        return true;
+    }
+    return false;
+}
+function qualifiedOpperator(val) {
+    if (userInput.length > 0) {
+        console.log("opp check");
+        if ( ["+","*","/","-"].some( el => userInput.slice(-1).includes(el) ) ) {
             deleteCharacter();
-            return val;
-
+            return(true);
+        }
+        let numbersSoFar = getNumbers(userInput);
+        let opperatorsSoFar = getOpperatorsSoFar();
+        let negativesSoFar = getSymbolsUsedSoFar("-");
+        // console.log("checking 2", opperatorsSoFar, neg);
+        if ( numbersSoFar.length < 2 && opperatorsSoFar.length < 1 && negativesSoFar.length < 2) {
+            return(true);
         }
     }
-    return "";
+    return false;
 }
 
-function addkeyClick( val ) {
-
-    // NUMBER: GO
-    // SYMBOL: DEPENDS ON PREV
-    //      PERCENT: GO
-    //      FIRST ANYTHING: GO
-    //      FIRST SYMBOL WITH DECIMAL: GO
-    //      SECOND DECIMAL, WITH EXISTING SYMBOL: GO
-    //      SYMBOL WITH NO PREV VALUE: PULL LINE-B, GO
-
-
-     // IT'S A NUMBER
-     if (parseInt(val).toString() == val ) {
-        // PREVIOUS IS NOT A PERCENT?
-        if (userInput[userInput.length-1] == "%" ) {
-
-        } else {
-            userlineAdd(val);     
-        }
-    } else {
-        // IT'S A SYMOBL
-        let currentInput = userInput.split(/[0-9]/).filter(i => i);
-        switch(val) {
-            case "%":
-                if (userInput.length<1) {
-                    // PULL PREVIOUS NUMBER 
-                    if (prevAnswer.length >0) {
-                        // userInput = prevAnswer + val;
-                        userlineAdd(prevAnswer + val);
-                    }
-                } else {
-                    let n = userInput[userInput.length-1];
-                    console.log(typeof n);
-                    if ( n== "%" | n=="." | parseInt(n).toString()==n ) {
-                        userlineAdd(val);
-                    }
-                };
-                break;
-            case ".":
-                if (userInput[userInput.length-1] != "%"){
-                    
-                    // let n = userInput.split(/[0-9%.]/).filter(i => i);
-                    let n = currentInput.filter(i=>i==".");
-                    
-                    if (n.length <1) {
-                        userlineAdd(val);
-                    } else if (n.length < 2 & currentInput.length > 1) {
-                        userlineAdd(val);
-                    }
-                }
-                break;
-                // break;
-            case "-": //ACCOUNT FOR NEGATIVES
-            case "+":
-            case "*":
-            case "/":
-                if (userInput.length<1) {
-                    // PULL PREVIOUS NUMBER
-                    if (prevAnswer.length >0) {
-                        // userInput = prevAnswer + val;
-                        userlineAdd(prevAnswer + val);
-                    }
-                } else {
-                    let n = userInput.split(/[0-9%.]/).filter(i => i);
-                    if ( ["-","+","*","/"].some( el => userInput[userInput.length-1].includes(el) ) ) {
-                        deleteCharacter();
-                        userlineAdd(val);
-                    } else if (n.length < 1) {
-                        userlineAdd(val);
-                    }
-                }
-                break;
-        }
-
+function qualifiedChar(val) {
+    // 
+    if( isNumber(val)){
+        return(qualifiedDigit(val));
     }
+
+    switch( val ) {
+
+        // IS DIGIT:
+        // case /^\d/.test(val):
+        //     console.log("is Digiti")
+        //     return(qualifiedDigit(val));
+        case "%":
+            return(qualifiedPercent(val));
+        case ".":
+            return(qualifiedDecimal(val));
+        case "-":
+            return(qualifiedNegative(val));
+        case "+":
+        case "*":
+        case "/":
+            return(qualifiedOpperator(val));
+        default:
+            return false;
+    }
+}
+
+function addKeyClick(val) {
+    console.log("checking..")
+    // VAL IS A SINGLE DIGIT
+    if(qualifiedChar(val)) {
+        userlineAdd(val);     
+    }
+    
 }
 
 function userInputClear() {
@@ -258,8 +264,9 @@ function deleteCharacter () {
 }
 function clearScreen() {
     userInputClear();
-    for (let i= 0; i < printLines.length-3; i++) {
-        printLines[i].textContent = "";
+    prevAnswer="";
+    for (let i= 0; i < printLines.length-2; i++) {
+        printLines[i].querySelector(".print-display").textContent = "";
     }
 }
 
@@ -268,7 +275,7 @@ function shiftUp() {
     let printDisplayB
 
     for (let i= 0; i < printLines.length-2; i++) {
-        console.log(i);
+        // console.log(i);
         printDisplayA = printLines[i].querySelector(".print-display");
         printDisplayB = printLines[i+2].querySelector(".print-display");
 
@@ -291,10 +298,10 @@ function manageFloat( val ) {
 
 function add (a,b) {
     console.log("adding")
-    return manageFloat(processInput(a)+processInput(b));
+    return manageFloat( manageFloat(processInput(a))+manageFloat(processInput(b)));
 };
 function subtract (a,b) {
-    return manageFloat(processInput(a)-processInput(b));
+    return manageFloat( manageFloat(processInput(a))-manageFloat(processInput(b)));
 };
 function multiply(a,b) {
     return manageFloat(processInput(a)*processInput(b));
@@ -304,10 +311,11 @@ function divide(a,b) {
 }
 
 function processInput( digit ) {
-
+    
     // GET PERCENTS
-    let perc = digit.split(/[0-9.]/).filter(i => i);
+    let perc = digit.split(/[0-9.-]/).filter(i => i);
     if (perc.length > 0) {
+        // MAKE ARRAY
         perc = perc[0].split("");
     }
     let number = digit.split(/[%]/).filter(i => i)[0];
@@ -315,39 +323,55 @@ function processInput( digit ) {
 
     perc.forEach( () => {
         number = manageFloat(number/=100);
-        // number = (number/=100).toFixed(12).toString()
-        // parseFloat( number );
-        // console.log( "number is " + number );
     }  );
-    // parseFloat(number.toFixed(8).toString())
     return parseFloat(number);
 }
+function getNumbers( val ) {
+    let numbers = val.split(/[^\d%.-]/).filter(i=>i);
+    let result = [];
+    numbers.forEach( el => {
+        while (el.length > 0) {
+            let num = "";
+            let n = el.length;
+            for (let i = 0; i<n; i++) {
+                num += el[0];
+                el= el.slice(1);
+                if( el.length > 0 && ["-","+","*","/"].some( v => el[0].includes(v) ) ) break;
+            }
+            result.push(num);    
+        }
+        result.push(el);
+    });
+    result=result.filter(i => i);
+    return result;
+};
+
+function getOpperations( val ) {
+    return val.split(/[0-9-.%]/).filter(i => i);
+};
 
 function calcSolution() {
     displayingAnswer = true;
     cursorVisibility(false);
     typeBoxB.textContent = userInput;
-
     typeBox.classList.remove("green");
     typeBox.classList.add("yellow");
     lineA.classList.add("right");
     lineB.classList.add("shift");
 
-    let numbers = userInput.split(/[^\d%.]/).filter(i=>i);
-    let operations = userInput.split(/[0-9.%]/).filter(i => i);
-    console.log("my recorded input is: ", userInput);
-    console.log("the numbers are ", numbers);
-    console.log("the operations are " + operations);
+    let numbers = getNumbers(userInput) //.split(/[^\d%.-]/).filter(i=>i);
+    let operations = getOpperations(userInput) //.split(/[0-9.%]/).filter(i => i);
     let answer = "";
 
     // EVALUATE OPPERATION
     // THERE ARE NO OPPERATIONS
     if (operations.length < 1 ) {
-        console.log("no opperations i guess");
         if (numbers.length < 1) {
             answer = processInput("0");
-        } else {
+        } else if (numbers.length < 2) {
             answer = processInput(userInput);
+        } else {
+            answer = add( numbers[0], numbers[1] );
         }
     } else {
         if (numbers.length < 2) {
